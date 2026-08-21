bf4d03fb870cf15a1938f1a601b018d87ab3279d
alishancodes <alishan2871@gmail.com>
2026-08-21 11:03:14 +0000

    added the hover effect

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const container = document.querySelector(".container");
 let lineCount = 1;
+let gridSize;
 
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < gridSize ; i++) {
     const parentLineDiv = document.createElement("div");
-    for (let k = 0; k < 16; k++) {
+    for (let k = 0; k < gridSize; k++) {
         const childDiv = document.createElement("div");
+        // 1. Triggers when the mouse moves ONTO the element
+        childDiv.addEventListener('mouseenter', () => {
+          childDiv.style.backgroundColor = 'purple';
+        });
+        
+        // 2. Triggers when the mouse MOVES AWAY from the element
+        childDiv.addEventListener('mouseleave', () => {
+          childDiv.style.backgroundColor = 'gray';
+        });
         parentLineDiv.append(childDiv);
     }
     parentLineDiv.classList.add("line", `${lineCount}`); lineCount++;
