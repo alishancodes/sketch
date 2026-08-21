fc3eb516af04e44e089fb88618e47e964058f59a
alishancodes <alishan2871@gmail.com>
2026-08-21 11:48:24 +0000

    Add dynamic grid generation and hover effects

--- script.js
+++ script.js
@@ -1,23 +1,48 @@
 const container = document.querySelector(".container");
 let lineCount = 1;
-let gridSize;
+
+const confirmBtn = document.createElement("button");
+confirmBtn.textContent = "Confirm";
+document.body.prepend(confirmBtn);
 
 
-for (let i = 0; i < gridSize ; i++) {
-    const parentLineDiv = document.createElement("div");
-    for (let k = 0; k < gridSize; k++) {
-        const childDiv = document.createElement("div");
-        // 1. Triggers when the mouse moves ONTO the element
-        childDiv.addEventListener('mouseenter', () => {
-          childDiv.style.backgroundColor = 'purple';
-        });
-        
-        // 2. Triggers when the mouse MOVES AWAY from the element
-        childDiv.addEventListener('mouseleave', () => {
-          childDiv.style.backgroundColor = 'gray';
-        });
-        parentLineDiv.append(childDiv);
+
+const gridSize = document.createElement("input");
+gridSize.id ="input";
+gridSize.type = "number";
+document.body.prepend(gridSize);
+
+
+
+confirmBtn.addEventListener("click", proceed);
+
+function proceed() {
+    let size = Number(gridSize.value);
+
+    if (Number.isNaN(size)) {
+        alert("Not a valid input!");
+        return;
     }
-    parentLineDiv.classList.add("line", `${lineCount}`); lineCount++;
-    container.append(parentLineDiv);
+    
+    console.log("runing");
+    for (let i = 0; i < size; i++) {
+        const parentLineDiv = document.createElement("div");
+        for (let k = 0; k < size; k++) {
+            const childDiv = document.createElement("div");
+            // 1. Triggers when the mouse moves ONTO the element
+            childDiv.addEventListener('mouseenter', () => {
+                childDiv.style.backgroundColor = 'purple';
+            });
+            
+            console.log("runing2");
+            // 2. Triggers when the mouse MOVES AWAY from the element
+            childDiv.addEventListener('mouseleave', () => {
+                childDiv.style.backgroundColor = 'gray';
+            });
+            parentLineDiv.append(childDiv);
+        }
+        parentLineDiv.classList.add("line", `${lineCount}`); lineCount++;
+        container.append(parentLineDiv);
+        console.log("runing3");
+    }
 }
